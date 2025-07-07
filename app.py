c3365039907a6e4b7f16aa8fdd7db63f99124640
aswin-sai <aswinsai1919@gmail.com>
2025-07-07 15:21:55 +0530

    first commit

--- app.py
+++ app.py
@@ -44,5 +44,10 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
+    # --- TEMPORARY: Create all tables if they do not exist ---
+    from extensions import db
+    with app.app_context():
+        db.create_all()
+    # --- Remove the above after tables are created ---
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
